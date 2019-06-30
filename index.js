18c06cee8f41877b8c87088d1a0494d9fb9ba0b5
Won Seob Shin <wonseob07@gmail.com>
2019-06-30 02:21:19 +0000

    passing all tests

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 function sumItems(array) {
   // Sum all the numbers in the array
+  let sum = 0;
+  array.forEach((element) => {
+    if(typeof element === 'number'){
+      sum += element;      
+    } else {
+      return sum += sumItems(element);
+    }
+  })
+  return sum;
 }
 
 module.exports = sumItems;
